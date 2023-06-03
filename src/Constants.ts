98ab81825e9b1b329267799fb1a93f93133d9992
KarterAZ <kazwetschke@gmail.com>
2023-06-03 02:22:04 -0700

    added vs to gitignore, fin part 10

---
+++ src/Constants.ts
@@ -0,0 +1,84 @@
+import white_pawn from './assets/images/pawn_w.png';
+import black_pawn from './assets/images/pawn_b.png';
+import white_rook from './assets/images/rook_w.png';
+import black_rook from './assets/images/rook_b.png';
+import white_knight from './assets/images/knight_w.png';
+import black_knight from './assets/images/knight_b.png';
+import white_bishop from './assets/images/bishop_w.png';
+import black_bishop from './assets/images/bishop_b.png';
+import white_queen from './assets/images/queen_w.png';
+import black_queen from './assets/images/queen_b.png';
+import white_king from './assets/images/king_w.png';
+import black_king from './assets/images/king_b.png';
+
+export const VERTICAL_AXIS = ["1", "2", "3", "4", "5", "6", "7", "8"];
+export const HORIZONTAL_AXIS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+export const GRID_SIZE = 100;
+
+export function samePosition(p1: Position, p2: Position) {
+    return p1.x === p2.x && p1.y === p2.y;
+}
+
+export enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    QUEEN,
+    KING
+}
+
+export enum TeamType {
+    OPPONENT,
+    OUR
+}
+
+export interface Position {
+    x: number;
+    y: number;
+}
+
+export interface Piece {
+    image: string;
+    position: Position;
+    type: PieceType;
+    team: TeamType;
+    enPassant?: boolean;
+}
+
+
+export const initialBoardState: Piece[] = [
+    { image: white_king, position: { x: 3, y: 0, }, type: PieceType.KING, team: TeamType.OUR },
+    { image: white_queen, position: { x: 4, y: 0, }, type: PieceType.QUEEN, team: TeamType.OUR },
+    { image: black_king, position: { x: 4, y: 7, }, type: PieceType.KING, team: TeamType.OPPONENT },
+    { image: black_queen, position: { x: 3, y: 7, }, type: PieceType.QUEEN, team: TeamType.OPPONENT },
+    { image: white_bishop, position: { x: 5, y: 0, }, type: PieceType.BISHOP, team: TeamType.OUR },
+    { image: white_bishop, position: { x: 2, y: 0, }, type: PieceType.BISHOP, team: TeamType.OUR },
+    { image: black_bishop, position: { x: 5, y: 7, }, type: PieceType.BISHOP, team: TeamType.OPPONENT },
+    { image: black_bishop, position: { x: 2, y: 7, }, type: PieceType.BISHOP, team: TeamType.OPPONENT },
+    { image: white_knight, position: { x: 6, y: 0, }, type: PieceType.KNIGHT, team: TeamType.OUR },
+    { image: white_knight, position: { x: 1, y: 0, }, type: PieceType.KNIGHT, team: TeamType.OUR },
+    { image: black_knight, position: { x: 6, y: 7, }, type: PieceType.KNIGHT, team: TeamType.OPPONENT },
+    { image: black_knight, position: { x: 1, y: 7, }, type: PieceType.KNIGHT, team: TeamType.OPPONENT },
+    { image: white_rook, position: { x: 7, y: 0, }, type: PieceType.ROOK, team: TeamType.OUR },
+    { image: white_rook, position: { x: 0, y: 0, }, type: PieceType.ROOK, team: TeamType.OUR },
+    { image: black_rook, position: { x: 7, y: 7, }, type: PieceType.ROOK, team: TeamType.OPPONENT },
+    { image: black_rook, position: { x: 0, y: 7, }, type: PieceType.ROOK, team: TeamType.OPPONENT },
+    { image: white_pawn, position: { x: 0, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 1, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 2, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 3, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 4, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 5, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 6, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: white_pawn, position: { x: 7, y: 1, }, type: PieceType.PAWN, team: TeamType.OUR },
+    { image: black_pawn, position: { x: 0, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 1, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 2, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 3, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 4, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 5, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 6, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+    { image: black_pawn, position: { x: 7, y: 6, }, type: PieceType.PAWN, team: TeamType.OPPONENT },
+];
--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,10 +1,10 @@
-import { PieceType, TeamType, Piece } from "../components/chessboard/Chessboard";
+import { PieceType, TeamType, Piece, Position } from "../Constants";
 
 export default class Referee 
 {
     tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean
     {
-        const piece = boardState.find(p => p.x === x && p.y === y);
+        const piece = boardState.find(p => p.position.x === x && p.position.y === y);
         if (piece)
             return true;
         return false;
@@ -12,7 +12,7 @@ export default class Referee
 
     tileIsOccupiedByOpponent(x: number, y: number, boardState: Piece[], team: TeamType): boolean
     {
-        const piece = boardState.find(p => p.x === x && p.y === y && p.team !== team);
+        const piece = boardState.find(p => p.position.x === x && p.position.y === y && p.team !== team);
 
         if (piece)
             return true;
@@ -20,10 +20,8 @@ export default class Referee
     }
 
     isEnPassantMove(
-        px: number, 
-        py: number, 
-        x: number, 
-        y: number, 
+        initialPosition: Position,
+        desiredPosition: Position,
         type: PieceType, 
         team: TeamType, 
         boardState: Piece[]): boolean
@@ -32,9 +30,9 @@ export default class Referee
 
         if (type === PieceType.PAWN)
         {
-            if ((x - px === -1 || x - px === 1) && y - py === pawnDirection)
+            if ((desiredPosition.x - initialPosition.x === -1 || desiredPosition.x - initialPosition.x === 1) && desiredPosition.y - initialPosition.y === pawnDirection)
             {
-                const piece = boardState.find(p => p.x === x && p.y === y - pawnDirection && p.enPassant);
+                const piece = boardState.find(p => p.position.x === desiredPosition.x && p.position.y === desiredPosition.y - pawnDirection && p.enPassant);
 
                 if (piece)
                 {
@@ -47,10 +45,8 @@ export default class Referee
     }
 
     isValidMove(
-        px: number, 
-        py: number, 
-        x: number, 
-        y: number, 
+        initialPosition: Position,
+        desiredPosition: Position,
         type: PieceType, 
         team: TeamType, 
         boardState: Piece[])
@@ -59,7 +55,7 @@ export default class Referee
 
         if (type === PieceType.PAWN)
         {
-            return this.pawnRule(px, py, x, y, team, boardState);
+            return this.pawnRule(initialPosition.x, initialPosition.y, desiredPosition.x, desiredPosition.y, team, boardState);
         }
 
         return false;
